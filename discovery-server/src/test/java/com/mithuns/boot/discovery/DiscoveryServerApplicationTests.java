07e3b4e7d97e1dd6d170bb49be2b3980dffe14ce
Mithun <info.mithun@gmail.com>
2021-10-31 21:38:47 +0530

    CLOUD-100

---
+++ discovery-server/src/test/java/com/mithuns/boot/discovery/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mithuns.boot.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
